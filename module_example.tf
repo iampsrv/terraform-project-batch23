7daa8695ecfceaefc5b2e43ce51b178904b0bc51
iampsrv <iampsrv@gmail.com>
2024-08-03 21:41:43 +0530

    commented module

--- module_example.tf
+++ module_example.tf
@@ -19,15 +19,15 @@
 #   public_ip_ec2 = true
 # }
 
-module "batch23mod" {
-  source            = "iampsrv/batch23mod/pranjal"
-  version           = "1.0.0"
-  cidr_block        = "10.0.0.0/16"
-  vpc_name          = "myvpc-tf"
-  subnet_cidr_block = "10.0.1.0/24"
-  sg_name           = "mysg-tf"
-  ami               = "ami-04a81a99f5ec58529"
-  instance_type     = "t2.micro"
-  instance_name     = "myec2tf"
-  public_ip_ec2     = true
-}
+# module "batch23mod" {
+#   source            = "iampsrv/batch23mod/pranjal"
+#   version           = "1.0.0"
+#   cidr_block        = "10.0.0.0/16"
+#   vpc_name          = "myvpc-tf"
+#   subnet_cidr_block = "10.0.1.0/24"
+#   sg_name           = "mysg-tf"
+#   ami               = "ami-04a81a99f5ec58529"
+#   instance_type     = "t2.micro"
+#   instance_name     = "myec2tf"
+#   public_ip_ec2     = true
+# }
